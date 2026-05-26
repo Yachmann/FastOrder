9857d0a57beac509ac19855d0b4e328f84c41b26
Yachmann <yachmanngustavo@gmail.com>
2026-05-26 11:57:46 -0300

    adjust : variaveis de ambiente para dbconnect

--- src/bd/connect.py
+++ src/bd/connect.py
@@ -1,16 +1,19 @@
+import os
+from dotenv import load_dotenv
 from rich.console import Console
 console = Console()
 import mysql.connector
 connector = mysql.connector
 
+load_dotenv()
 
 def conectar_db():
     try:
         db_connector = connector.connect(
-            host = "localhost",
-            user = "root",
-            passwd = "root",
-            database = "restaurante",
+            host = os.getenv("HOST"),
+            user = os.getenv("USER"),
+            passwd = os.getenv("PASSWORD"),
+            database = os.getenv("DATABASE"),
         )
         console.print("[green]Conexao com o banco de dados feita com sucesso[/green]")
         return db_connector
